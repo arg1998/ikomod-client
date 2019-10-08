3ecee37da2e6572225e61b100df007e65fa5d795
Amir Reza <arg7971@gmail.com>
2019-10-08 21:34:24 +0330

    added theme data

--- lib/utils/Theme.dart
+++ lib/utils/Theme.dart
@@ -1,6 +1,21 @@
 import 'package:flutter/material.dart';
 
-ThemeData generateTheme(BuildContext context) {
- 
-}
+const MaterialColor primarySwatch = Colors.pink;
+const TextTheme textTheme = TextTheme(title: TextStyle(fontSize: 22));
 
+ThemeData generateTheme(BuildContext context) {
+  ThemeData appTheme = Theme.of(context);
+
+  return ThemeData(
+    primarySwatch: primarySwatch,
+    appBarTheme: AppBarTheme(
+      textTheme: TextTheme(
+        title: TextStyle(
+          fontSize: 23,
+          fontFamily: "yekan",
+          fontWeight: FontWeight.w700,
+        ),
+      ),
+    ),
+  );
+}
