1fec26e7ca8a9f7bec9fca184fe01e161f115b5f
Amir Reza <arg7971@gmail.com>
2019-10-12 14:52:10 +0330

    edited theme file

--- lib/utils/Theme.dart
+++ lib/utils/Theme.dart
@@ -1,19 +1,25 @@
 import 'package:flutter/material.dart';
 
 const MaterialColor primarySwatch = Colors.pink;
-const TextTheme textTheme = TextTheme(title: TextStyle(fontSize: 22));
 
 ThemeData generateTheme(BuildContext context) {
   ThemeData appTheme = Theme.of(context);
 
+  TextTheme textTheme = appTheme.textTheme.copyWith(
+    title: TextStyle(fontFamily: "ziba"),
+    headline: TextStyle(fontFamily: "ziba", fontWeight: FontWeight.w600),
+    // subhead: TextStyle(fontSize: 20, fontFamily: "ziba", height: 1.5)
+  );
+
   return ThemeData(
     primarySwatch: primarySwatch,
+    textTheme: textTheme,
     appBarTheme: AppBarTheme(
       textTheme: TextTheme(
         title: TextStyle(
-          fontSize: 23,
-          fontFamily: "yekan",
-          fontWeight: FontWeight.w700,
+          fontSize: 30,
+          fontFamily: "ziba",
+          fontWeight: FontWeight.w900,
         ),
       ),
     ),
