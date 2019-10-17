3e96bf5e9aa9269ea1b9119491c19cc802686449
Amir Reza <arg7971@gmail.com>
2019-10-17 18:18:46 +0330

    refactored home app bar

--- lib/widgets/HomeAppBar.dart
+++ lib/widgets/HomeAppBar.dart
@@ -1,40 +1,53 @@
 import 'package:flutter/material.dart';
+import 'Coin.dart';
+import 'HomeMenuModal.dart';
+import 'IconImage.dart';
 
-class HomeAppBar extends StatelessWidget {
-  const HomeAppBar({
-    Key key,
-  }) : super(key: key);
+AppBar buildHomeAppBar(BuildContext context) {
+  final Size screenSize = MediaQuery.of(context).size;
+
+  return AppBar(
+    titleSpacing: 0,
+    centerTitle: true,
+    leading: IconButton(
+      icon: Icon(Icons.menu),
+      onPressed: () async {
+        showDialog(
+          context: context,
+          builder: (ctx) {
+            return HomeMenuModal(screenSize: screenSize);
+          },
+        );
+      },
+    ),
+    title: HomeAppBarTitle(
+      coins: "۱۶۳۸",
+    ),
+    actions: [
+      IconImage(
+        onTap: () {},
+        icon: IconImageAsset.search,
+        size: 24,
+      ),
+      IconImage(
+        icon: IconImageAsset.bell,
+        size: 24,
+      ),
+    ],
+  );
+}
+
+class HomeAppBarTitle extends StatelessWidget {
+  final String coins;
+
+  const HomeAppBarTitle({Key key, this.coins = "۳۴۷۱"}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Row(
       crossAxisAlignment: CrossAxisAlignment.center,
       children: [
-        Container(
-          padding: EdgeInsets.all(3),
-          decoration: BoxDecoration(
-            color: Colors.white,
-            borderRadius: BorderRadius.circular(20),
-          ),
-          child: Row(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              SizedBox(width: 5),
-              Text(
-                "۲۳۵۰",
-                style: TextStyle(
-                    fontSize: 15,
-                    fontFamily: "iransans",
-                    color: Color(0XFFff9500).withOpacity(0.9)),
-              ),
-              SizedBox(width: 5),
-              Image.asset(
-                "assets/images/coin-small.png",
-                height: 25,
-              ),
-            ],
-          ),
-        ),
+        Coin(coins: coins),
         Spacer(),
         Text("آی کمد"),
         Spacer(),
