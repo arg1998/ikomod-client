4a5e71e93b5e185e91b47dbecaeeed9fa3d35ab6
Amir Reza <arg7971@gmail.com>
2019-11-03 14:31:37 +0330

    added more icons to this calss

--- lib/widgets/IconImage.dart
+++ lib/widgets/IconImage.dart
@@ -42,29 +42,55 @@ class IconImageAsset {
   String get iconUrl => _url;
 
 // list of all images in assets bundle
-  static IconImageAsset get bell => IconImageAsset("assets/images/bell-small.png");
+  static IconImageAsset get bell =>
+      IconImageAsset("assets/images/bell-small.png");
   static IconImageAsset get search =>
       IconImageAsset("assets/images/search-small.png");
   static IconImageAsset get filter =>
       IconImageAsset("assets/images/filter-small.png");
   static IconImageAsset get shopping =>
       IconImageAsset("assets/images/shopping-small.png");
-  static IconImageAsset get ads => IconImageAsset("assets/images/ads-small.png");
+  static IconImageAsset get ads =>
+      IconImageAsset("assets/images/ads-small.png");
   static IconImageAsset get billboard =>
       IconImageAsset("assets/images/billboard-small.png");
   static IconImageAsset get camera =>
-      IconImageAsset("assets/images/camera-small.png");
-  static IconImageAsset get chat => IconImageAsset("assets/images/chat-small.png");
-  static IconImageAsset get gears => IconImageAsset("assets/images/gears-small.png");
+      IconImageAsset("assets/images/camera.png");
+  static IconImageAsset get chat =>
+      IconImageAsset("assets/images/chat-small.png");
+  static IconImageAsset get gears =>
+      IconImageAsset("assets/images/gears-small.png");
   static IconImageAsset get growth =>
       IconImageAsset("assets/images/growth-small.png");
   static IconImageAsset get article =>
       IconImageAsset("assets/images/book-small.png");
-  static IconImageAsset get plus => IconImageAsset("assets/images/plus-small.png");
+  static IconImageAsset get plus =>
+      IconImageAsset("assets/images/plus-small.png");
   static IconImageAsset get support =>
       IconImageAsset("assets/images/support-small.png");
   static IconImageAsset get trophy =>
       IconImageAsset("assets/images/trophy-small.png");
-      static IconImageAsset get cup =>
+  static IconImageAsset get cup =>
       IconImageAsset("assets/images/cup-large.png");
+  static IconImageAsset get logo =>
+      IconImageAsset("assets/images/splash_logo.png");
+
+  static IconImageAsset get editSmall =>
+      IconImageAsset("assets/images/edit-small.png");
+  static IconImageAsset get editLarge =>
+      IconImageAsset("assets/images/edit-large.png");
+  static IconImageAsset get goldMedalSmall =>
+      IconImageAsset("assets/images/medal-gold-small.png");
+  static IconImageAsset get goldMedalLarge =>
+      IconImageAsset("assets/images/medal-gold-large.png");
+
+  static IconImageAsset get silverMedalSmall =>
+      IconImageAsset("assets/images/medal-silver-small.png");
+  static IconImageAsset get silverMedalLarge =>
+      IconImageAsset("assets/images/medal-silver-large.png");
+
+  static IconImageAsset get bronzeMedalSmall =>
+      IconImageAsset("assets/images/medal-bronze-small.png");
+  static IconImageAsset get bronzeMedalLarge =>
+      IconImageAsset("assets/images/medal-bronze-large.png");
 }
